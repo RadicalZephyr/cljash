7500bacada42baf2294b304f2b5bc3c6d41aa7d0
Geoff Shannon <geoffpshannon@gmail.com>
2017-04-09 21:45:16 -0500

    Implement basic fork-and-exec behavior with ProcessMaker

--- src/cljash/cli.clj
+++ src/cljash/cli.clj
@@ -1,6 +1,28 @@
 (ns cljash.cli
   (:require [boot.core :as boot]
-            [boot.util :as util]))
+            [boot.util :as util])
+  (:import
+   (java.io BufferedReader
+            InputStreamReader
+            IOException)))
+
+(defn prompt-and-read! [reader]
+  (.print System/err "%% ")
+  (.readLine reader))
+
+(defn process-line [line]
+  (let [process-builder (-> (ProcessBuilder. (into-array String [line]))
+                            )]
+    (try
+      (.start process-builder)
+      (catch IOException e
+        (.printf System/err "error: %s\n" (into-array Object (.getMessage e)))))))
 
 (defn cli []
-  (util/dosh "ls" "-lah" "--color=always"))
+  (with-open [reader (-> System/in
+                         InputStreamReader.
+                         BufferedReader.)]
+    (loop []
+      (when-let [line (prompt-and-read! reader)]
+        (process-line line)
+        (recur)))))
